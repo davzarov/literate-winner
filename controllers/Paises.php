9c4ea649fa0671fe3c9cb0e7011e84e446d1196c
davzarov <davidzarate2590@gmail.com>
2020-06-22 10:07:04 -0400

    paises, personas controllers

---
+++ controllers/Paises.php
@@ -0,0 +1,82 @@
+<?php
+    class Paises extends Controller {
+
+        private $paisModel;
+
+        public function __CONSTRUCT()
+        {
+            $this->paisModel = $this->model('Pais');
+        }
+
+        public function index()
+        {
+            $context = [
+                'paises' => $this->paisModel->Listar()
+            ];
+            $this->view('pais/index', $context);
+        }
+
+        public function ver($pais_codigo)
+        {
+            $pais = $this->paisModel->Obtener($pais_codigo);
+            $context = [
+                'pais' => $pais
+            ];
+            $this->view('pais/editar', $context);
+        }
+
+        public function nuevo()
+        {
+            $context = [
+                'pais' => new Pais()
+            ];
+            $this->view('pais/nuevo', $context);
+        }
+
+        public function guardar()
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $pais = new Pais();
+                $pais->pais_descripcion = $_POST['pais_descripcion'];
+                
+                if($this->paisModel->Registrar($pais)) {
+                    flash('pais_mensaje', 'Se ha agregado correctamente.');
+                    redirect('paises');
+                } else {
+                    die('Ha ocurrido un error.');
+                }
+            }
+        }
+
+        public function editar($pais_codigo)
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $pais = new Pais();
+                $pais->pais_codigo = $pais_codigo;
+                $pais->pais_descripcion = $_POST['pais_descripcion'];
+
+                if($this->paisModel->Actualizar($pais)) {
+                    flash('pais_mensaje', 'Se ha modificado correctamente.');
+                    redirect('paises');
+                } else {
+                    die('Ha ocurrido un error.');
+                }
+            }
+        }
+
+        public function eliminar($pais_codigo)
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                if($this->paisModel->Eliminar($pais_codigo)) {
+                    flash('pais_mensaje', 'Se ha eliminado correctamente.');
+                    redirect('paises');
+                } else {
+                    die('Ha ocurrido un error.');
+                }
+            }
+        }
+    }
---
+++ controllers/Personas.php
@@ -0,0 +1,112 @@
+<?php
+    class Personas extends Controller {
+
+        private $tipoPersonaModel;
+        private $tipoDocumentoModel;
+        private $generoModel;
+        private $barrioModel;
+        private $personaModel;
+
+        public function __CONSTRUCT()
+        {
+            $this->tipoPersonaModel = $this->model('TipoPersona');
+            $this->tipoDocumentoModel = $this->model('TipoDocumento');
+            $this->generoModel = $this->model('Genero');
+            $this->barrioModel = $this->model('Barrio');
+            $this->personaModel = $this->model('Persona');
+        }
+
+        public function index()
+        {
+            $context = [
+                'personas' => $this->personaModel->Listar()
+            ];
+            $this->view('persona/index', $context);
+        }
+
+        public function ver($persona_codigo)
+        {
+            $persona = $this->personaModel->Obtener($persona_codigo);
+            $context = [
+                'persona' => $persona,
+                'tipos_persona' => $this->tipoPersonaModel->Listar(),
+                'tipos_documento' => $this->tipoDocumentoModel->Listar(),
+                'generos' => $this->generoModel->Listar(),
+                'barrios' => $this->barrioModel->Listar()
+            ];
+            $this->view('persona/editar', $context);
+        }
+
+        public function nuevo()
+        {
+            $context = [
+                'persona' => new Persona(),
+                'tipos_persona' => $this->tipoPersonaModel->Listar(),
+                'tipos_documento' => $this->tipoDocumentoModel->Listar(),
+                'generos' => $this->generoModel->Listar(),
+                'barrios' => $this->barrioModel->Listar()
+            ];
+            $this->view('persona/nuevo', $context);
+        }
+
+        public function guardar()
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $persona = new Persona();
+                $persona->persona_nombre1 = $_POST['persona_nombre1'];
+                $persona->persona_nombre2 = $_POST['persona_nombre2'];
+                $persona->persona_apellido1 = $_POST['persona_apellido1'];
+                $persona->persona_apellido2 = $_POST['persona_apellido2'];
+                $persona->tipo_persona_codigo = $_POST['tipo_persona_codigo'];
+                $persona->tipo_documento_codigo = $_POST['tipo_documento_codigo'];
+                $persona->genero_codigo = $_POST['genero_codigo'];
+                $persona->barrio_codigo = $_POST['barrio_codigo'];
+
+                if($this->personaModel->Registrar($persona)) {
+                    flash('persona_mensaje', 'Se ha agregado correctamente.');
+                    redirect('personas');
+                } else {
+                    die('Ha ocurrido un error.');
+                }
+            }
+        }
+
+        public function editar($persona_codigo)
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+                $persona = new Persona();
+                $persona->persona_codigo = $persona_codigo;
+                $persona->persona_nombre1 = $_POST['persona_nombre1'];
+                $persona->persona_nombre2 = $_POST['persona_nombre2'];
+                $persona->persona_apellido1 = $_POST['persona_apellido1'];
+                $persona->persona_apellido2 = $_POST['persona_apellido2'];
+                $persona->tipo_persona_codigo = $_POST['tipo_persona_codigo'];
+                $persona->tipo_documento_codigo = $_POST['tipo_documento_codigo'];
+                $persona->genero_codigo = $_POST['genero_codigo'];
+                $persona->barrio_codigo = $_POST['barrio_codigo'];
+
+                if($this->personaModel->Actualizar($persona)) {
+                    flash('persona_mensaje', 'Se ha modificado correctamente.');
+                    redirect('personas');
+                } else {
+                    die('Ha ocurrido un error.');
+                }
+            }
+        }
+
+        public function eliminar($persona_codigo)
+        {
+            if($_SERVER['REQUEST_METHOD'] == 'POST') {
+                if($this->personaModel->Eliminar($persona_codigo)) {
+                    flash('persona_mensaje', 'Se ha eliminado correctamente.');
+                    redirect('personas');
+                } else {
+                    die('Ha ocurrido un error.');
+                }
+            }
+        }
+    }
